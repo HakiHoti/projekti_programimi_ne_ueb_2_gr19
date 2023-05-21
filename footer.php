c4eb9eb6687955d3371db0014680f0a7843054f4
Haki Hoti <122534928+HakiHoti@users.noreply.github.com>
2023-05-21 15:06:06 -0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,22 @@
+<footer id="footer" class="kilimanjaro_area">
+                    
+                    <div style="color:whitesmoke">
+                        <hr>
+                        <h5>About Us</h5>
+                        <p>It includes rich features & contents. It's designed & developed based on One Page/ Multi-page Layout,blog themes,world press themes and blogspot. You can use any layout from any demo anywhere.</p>
+                        <p>Our company is completely creative, clean & 100% responsive website. Put your business into next level with us.</p>
+                    </div>
+                    <div >
+                        <h5 style="color:whitesmoke">Social Links:</h5>
+                        
+                        <ul style="display:flex;gap:20px;">
+                            <li class="li"><a href="https://www.facebook.com/"><i class="fa fa-facebook" aria-hidden="true"></i> Facebook</a></li>
+                            <li class="li"><a href="https://twitter.com/"><i class="fa fa-twitter" aria-hidden="true"></i> Twitter</a></li>
+                            <li class="li"><a href="https://www.pinterest.com/pin/636485359830064724/"><i class="fa fa-pinterest" aria-hidden="true"></i> Pinterest</a></li>
+                            <li class="li"><a href="https://www.youtube.com/"><i class="fa fa-youtube" aria-hidden="true"></i> YouTube</a></li>
+                            <li class="li"><a href="https://www.linkedin.com/feed/"><i class="fa fa-linkedin" aria-hidden="true"></i> Linkedin</a></li>
+                        </ul>
+                        </div>
+                        </div>
+                        </div>
+</footer>
